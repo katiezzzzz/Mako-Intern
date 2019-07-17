8d19d485225f318ea46f5a0933e80cba38f1c6a3
katiezzzzz <47574900+katiezzzzz@users.noreply.github.com>
2019-07-17 18:37:50 +0100

    Add files via upload

---
+++ 17.07.2019/API.py
@@ -0,0 +1,7 @@
+'''
+- APIs are hosted on web servers
+- data is usually returned in JSON format
+- an endpoint is a server route that is used to retrieve different data from the API
+'''
+
+# iss-now.json gets the current latitude and longitude of the International Space
